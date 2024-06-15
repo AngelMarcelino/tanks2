e46f7a95badeab4343b278d93ec4fb34554f77b1
Marcelino Gonzalez <agemarcelino@hotmail.com>
2024-06-15 15:52:35 -0600

    Add acceleration

--- Tanks/Controlls/Controll.cs
+++ Tanks/Controlls/Controll.cs
@@ -9,13 +9,22 @@ namespace Tanks.Controlls
 {
     internal class Controll
     {
-        public Controll(KeyBindings keyBindings) 
+        public Controll(KeyBindings keyBindings)
         {
             _keyBindings = keyBindings;
             GamePlayActionState = GetInitialDictionary();
         }
         private KeyBindings _keyBindings;
         public Dictionary<GameplayAction, bool> GamePlayActionState { get; }
+
+
+        public static HashSet<GameplayAction> MovementGamePlayActions { get; } = new HashSet<GameplayAction>()
+        {
+            GameplayAction.Up,
+            GameplayAction.Down,
+            GameplayAction.Left,
+            GameplayAction.Right,
+        };
 
         public void SetControll(VirtualKey key)
         {
--- Tanks/Tank.cs
+++ Tanks/Tank.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using Tanks.Controlls;
+using Tanks.Physics;
 using Windows.Foundation;
 using Windows.Media.Devices.Core;
 
@@ -13,9 +14,37 @@ namespace Tanks
 {
     class Tank
     {
-        public const double HighSpeed = 3000;
-        public const double NormalSpeed = 1500;
-        public double Speed { get; set; } = HighSpeed;
+        public const double Acceleration = 30;
+        public const double BrakingSpeed = 300;
+        public const double Friction = 50;
+        public const double BaseSpeed = 20;
+        private double _xSpeed = 0;
+        private double _ySpeed = 0;
+        public double MaxSpeed = BaseSpeed;
+        public double MinSpeed = -BaseSpeed;
+        public const double SpeedMultiplier = 1.5;
+        public const bool Vertical = true;
+        public const bool Horizontal = false;
+        public const double AccelerationMultiplier = 2;
+
+        public double XSpeed
+        {
+            get { return _xSpeed; }
+            set
+            {
+                _xSpeed = LimitSpeeds(value);
+            }
+        }
+
+        public double YSpeed
+        {
+            get { return _ySpeed; }
+            set
+            {
+                _ySpeed = LimitSpeeds(value);
+            }
+        }
+
         public double X { get; set; } = 40; 
         public double Y { get; set; } = 40;
         public double Widht { get; set; } = 40;
@@ -30,35 +59,133 @@ namespace Tanks
 
         public void UpdateState()
         {
+            UpdateSpeed();
+            UpdatePosition();
+        }
+
+        private double DecreaseAbsoluteSpeedToZero(double speed, double acceleration)
+        {
+            if (speed == 0) return 0;
+            if (speed < 0)
+            {
+                speed = speed + acceleration;
+                if (speed > 0)
+                {
+                    speed = 0;
+                }
+            }
+            if (speed > 0)
+            {
+                speed = speed - acceleration;
+                if (speed < 0)
+                {
+                    speed = 0;
+                }
+            }
+            return speed;
+        }
+
+        private double LimitSpeeds(double inputSpeed)
+        {
+            double _speed = 0;
+            if (inputSpeed < MinSpeed)
+            {
+                _speed = MinSpeed;
+            }
+            else if (inputSpeed > MaxSpeed)
+            {
+                _speed = MaxSpeed;
+            }
+            else
+            {
+                _speed = inputSpeed;
+            }
+            return _speed;
+        }
+
+        private bool IsMoving()
+        {
+            return Controll.MovementGamePlayActions.Any(controll.GetActionState);
+        }
+
+        private void UpdateSpeed()
+        {
             double deltaX = 0;
             double deltaY = 0;
-            if (controll.GetActionState(GameplayAction.Speed))
-            {
-                Speed = HighSpeed;
-            } else
-            {
-                Speed = NormalSpeed;
-            }
-
+            double speedMultiplier = 1;
             if (controll.GetActionState(GameplayAction.Up))
             {
-                deltaY += -Speed * GlobalTimer.ElapsedTimeInSeconds;
+                if (YSpeed > 0)
+                {
+                    Brake(Direction.Vertical);
+                }
+                deltaY += -Acceleration * GlobalTimer.ElapsedTimeInSeconds;
+            }
+            if (controll.GetActionState(GameplayAction.Down))
+            {
+                if (YSpeed < 0)
+                {
+                    Brake(Direction.Vertical);
+                }
+                deltaY += Acceleration * GlobalTimer.ElapsedTimeInSeconds;
+            }
+            if (!controll.GetActionState(GameplayAction.Up) && !controll.GetActionState(GameplayAction.Down))
+            {
+                YSpeed = DecreaseAbsoluteSpeedToZero(YSpeed, Friction * GlobalTimer.ElapsedTimeInSeconds);
+            }
+            if (controll.GetActionState(GameplayAction.Left))
+            {
+                if (XSpeed > 0)
+                {
+                    Brake(Direction.Horizontal);
+                }
+                deltaX += -Acceleration * GlobalTimer.ElapsedTimeInSeconds;
+            }
+            if (controll.GetActionState(GameplayAction.Right))
+            {
+                if (XSpeed < 0)
+                {
+                    Brake(Direction.Horizontal);
+                }
+                deltaX += Acceleration * GlobalTimer.ElapsedTimeInSeconds;
+            }
+            if (!controll.GetActionState(GameplayAction.Left) && !controll.GetActionState(GameplayAction.Right))
+            {
+                XSpeed = DecreaseAbsoluteSpeedToZero(XSpeed, Friction * GlobalTimer.ElapsedTimeInSeconds);
             }
 
-            if(controll.GetActionState(GameplayAction.Down))
+            if (controll.GetActionState(GameplayAction.Speed))
             {
-                deltaY += Speed * GlobalTimer.ElapsedTimeInSeconds;
+                speedMultiplier = AccelerationMultiplier;
+                MaxSpeed = BaseSpeed * SpeedMultiplier;
+                MinSpeed = -BaseSpeed * SpeedMultiplier;
             }
+            else
+            {
+                MaxSpeed = BaseSpeed;
+                MinSpeed = -BaseSpeed;
+            }
+            
+            XSpeed += deltaX * speedMultiplier;
+            YSpeed += deltaY * speedMultiplier;
+        }
 
-            if(controll.GetActionState(GameplayAction.Left))
+        private void Brake(Direction direction)
+        {
+            if (direction == Direction.Vertical)
             {
-                deltaX += -Speed * GlobalTimer.ElapsedTimeInSeconds;
+                YSpeed = DecreaseAbsoluteSpeedToZero(YSpeed, BrakingSpeed * GlobalTimer.ElapsedTimeInSeconds);
             }
+            else
+            {
+                XSpeed = DecreaseAbsoluteSpeedToZero(XSpeed, BrakingSpeed * GlobalTimer.ElapsedTimeInSeconds);
+            }
+        }
 
-            if(controll.GetActionState(GameplayAction.Right))
-            {
-                deltaX += Speed * GlobalTimer.ElapsedTimeInSeconds;
-            }
+        private void UpdatePosition()
+        {
+            double deltaX = XSpeed;
+            double deltaY = YSpeed;
 
             X += deltaX;
             Y += deltaY;
@@ -75,6 +202,8 @@ namespace Tanks
         public void Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
             args.DrawingSession.FillRectangle(Shape, Microsoft.UI.Colors.White);
+            args.DrawingSession.DrawText($"XSpeed: {XSpeed}", 40, 40, Microsoft.UI.Colors.AliceBlue);
+            args.DrawingSession.DrawText($" YSpeed: {YSpeed}", 40, 80, Microsoft.UI.Colors.AliceBlue);
         }
     }
 }
